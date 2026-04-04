873326793670f134f1811800758d4c93b8f84b31
Kishor A R <kishortkd7@gmail.com>
2026-04-04 18:56:22 +0530

    fix: configured tailwindcss to the project

---
+++ src/app/app.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './app.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1 className='text-3xl font-bold underline text-center text-blue-500'>Hello world!</h1>
+    </>
+  )
+}
+
+export default App
